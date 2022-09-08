dcebc7434c2ad51128e008648f935bec75deccac
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-09-07 22:27:37 -0300

    feat: add function to display book info

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+
+    this.info = function(){
+        if(this.read){
+            return `${this.title} by ${this.author}, ${pages} pages, already read`;
+        }
+        return `${this.title} by ${this.author}, ${pages} pages, not read yet`;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read){
     const book = new Book(title, author, pages, read);
 
     myLibrary.push(book);
+}
+
+function displayBooks(){
+    for(const book of myLibrary){
+        console.log(book.info());
+    }
 }
